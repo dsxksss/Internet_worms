9c55b378b9d5ff19cacf02b2e2de63c8d4cc5912
VentCat <2546650292@qq.com>
2023-07-26 14:47:49 +0800

    优化代码

--- jxsl/xsjbxx.py
+++ jxsl/xsjbxx.py
@@ -2,9 +2,23 @@ import requests
 import json
 import csv
 import asyncio
+import os
 from fake_useragent import UserAgent
 
+
+# 获取当前执行此程序的目录并且存放起来后续代码需要在此基础上拼接
+WORKPATH = os.getcwd()
+CSVPATH = f"{WORKPATH}/江西水利学生信息.csv"
+
+
+# 判断是否存在csv文件
+if not os.path.exists(CSVPATH):
+    with open(CSVPATH, "w", newline="", encoding="utf-8") as w:
+        writer = csv.DictWriter(w, fieldnames=["创建时间", "所在班级", "学号", "姓名", "所在系"])
+        writer.writeheader()
+
 url = "http://portal.jxslsd.com:4106/api/upp/contentDisplay/queryAppointCard/abd7305a3a1347e596c4f761b41f4e57"
+
 
 print("请输入你要爬取的学号区间")
 start = int(input("开始学号:"))
@@ -20,20 +34,15 @@ async def getUserData(loginuserid):
         "Connection": "keep-alive",
     }
 
-    response = requests.request("GET", url, headers=headers, data={})
+    response = requests.request("GET", url, headers=headers, data=b"")
     result = json.loads(response.text)
     data = json.loads(result["data"]["data"])
     return data[0]
 
 
-data_list = []
-with open("./21级学生信息.csv", "w", newline="", encoding="utf-8") as w:
-    writer = csv.DictWriter(w, fieldnames=["创建时间", "所在班级", "学号", "姓名", "所在系"])
-    writer.writeheader()
-
-
 async def main():
-    userids = [str(i) for i in range(start, end+1)]
+    data_couter = 0
+    userids = [str(i) for i in range(start, end + 1)]
 
     for userid in userids:
         try:
@@ -45,21 +54,24 @@ async def main():
                 "姓名": pure_user["YHMC"],
                 "所在系": pure_user["ZY"],
             }
-            with open("./21级学生信息.csv", "a", newline="", encoding="utf-8") as w:
+            # 解码并保存学生数据
+            with open(CSVPATH, "a", newline="", encoding="utf-8") as w:
                 writer = csv.DictWriter(
                     w, fieldnames=["创建时间", "所在班级", "学号", "姓名", "所在系"]
                 )
                 writer.writerow(user)
-            data_list.append(user)
-            # 显示已经保存了多少张图片的提示信息
+
+            # 显示已经保存了多少个用户的提示信息
+            data_couter += 1
             print(
-                f"\r\033[31m已保存\033[0m[\033[32m{len(data_list)}\033[0m]\033[31m个用户\033[0m",
+                f"\r\033[31m已保存\033[0m[\033[32m{data_couter}\033[0m]\033[31m个用户\033[0m",
                 end="",
             )
+
         except:
             continue
 
-    print("\n\033[32m爬取完毕!\033[0m")
+    print("\n\033[32m获取完毕!\033[0m")
 
 
 # 运行异步函数
